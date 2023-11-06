9fbe3f6ceb7e00344fe463561484f1d7b2f48f6f
kubraacelik <kkbra.celik92@gmail.com>
2023-11-06 23:32:04 +0300

    average finding calculated

---
+++ averageFinder/averageFinder.js
@@ -0,0 +1,11 @@
+let midterm1 = Number(prompt("Enter your midterm 1 grade : "));
+let midterm2 = Number(prompt("Enter your midterm 2 grade : "));
+let final = Number(prompt("Enter your final grade : "));
+
+let average = midterm1 * 0.3 + midterm2 * 0.3 + final * 0.5;
+
+if (average >= 35) {
+  alert("You have successfully passed the course. Average :  " + average);
+} else {
+  alert("You failed the class. Average :  " + average);
+}
