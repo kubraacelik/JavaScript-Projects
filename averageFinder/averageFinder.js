ff34a4c7e1409594ec2ae2ebd0c04413c2d8f2b4
kubraacelik <kkbra.celik92@gmail.com>
2023-11-07 10:49:14 +0300

    the percentage affecting the final was changed

--- averageFinder/averageFinder.js
+++ averageFinder/averageFinder.js
@@ -2,7 +2,7 @@ let midterm1 = Number(prompt("Enter your midterm 1 grade : "));
 let midterm2 = Number(prompt("Enter your midterm 2 grade : "));
 let final = Number(prompt("Enter your final grade : "));
 
-let average = midterm1 * 0.3 + midterm2 * 0.3 + final * 0.5;
+let average = midterm1 * 0.3 + midterm2 * 0.3 + final * 0.4;
 
 if (average >= 35) {
   alert("You have successfully passed the course. Average :  " + average);
