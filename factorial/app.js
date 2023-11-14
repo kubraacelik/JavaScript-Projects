6f015a1a52ba2260f5892d89c4e6904951190e23
kubraacelik <kkbra.celik92@gmail.com>
2023-11-15 00:23:06 +0300

    factorial calculation made

---
+++ factorial/app.js
@@ -0,0 +1,7 @@
+let number = Number(prompt("Enter the number : "));
+let multiplication = 1;
+
+for (let i = 1; i <= number; i++) {
+  multiplication = multiplication * i;
+}
+alert("Result : " + multiplication);
