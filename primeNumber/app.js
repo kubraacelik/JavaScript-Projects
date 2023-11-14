a68b25aababb2389992d73f5d0ff2b6ad186ebd6
kubraacelik <kkbra.celik92@gmail.com>
2023-11-15 00:16:08 +0300

    comment line added

--- primeNumber/app.js
+++ primeNumber/app.js
@@ -1,6 +1,7 @@
 let number = Number(prompt("Enter the number : "));
 let result = true;
 
+//Not prime
 for (let i = 2; i <= Math.floor(number / 2); i++) {
   if (number % i == 0) {
     result = false;
