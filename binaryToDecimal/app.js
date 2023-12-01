463ebe37b2bf8ac6b3d12f46596a92d952762139
kubraacelik <kkbra.celik92@gmail.com>
2023-12-01 16:32:59 +0300

    added run warning if binary is not 0

--- binaryToDecimal/app.js
+++ binaryToDecimal/app.js
@@ -3,8 +3,10 @@ let binary = "1010111";
 function binaryToDecimal(binary) {
   let toplam = 0;
   let ust = 0;
+
   for (let i = binary.length - 1; i >= 0; i--) {
     if (Number(binary.charAt(i)) != 0) {
+      //binary 0 değilse çalıştır
       toplam += Number(binary.charAt(i)) * Math.pow(2, ust);
     }
     ust++;
