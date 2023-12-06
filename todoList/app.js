e476d1d6e02c46ce5d90e78ba211d74abf34c9d6
kubraacelik <kkbra.celik92@gmail.com>
2023-12-06 21:02:54 +0300

    updated

--- todoList/app.js
+++ todoList/app.js
@@ -1,3 +1,45 @@
 const form = document.querySelector("#todoAddForm");
 const addInput = document.querySelector("#todoName");
 const todoList = document.querySelector(".list-group");
+const firstCardBody = document.querySelectorAll(".card-body")[0];
+const secondCardBody = document.querySelectorAll(".card-body")[1];
+const clearButton = document.querySelector("#clearButton");
+
+runEvents();
+
+//form'da submit yapınca addTodo fonksiyonum çalışsın
+function runEvents() {
+  form.addEventListener("submit", addTodo);
+}
+
+function addTodo(e) {
+  const inputText = addInput.value.trim();
+
+  if (inputText == null || inputText == "") {
+    alert("Lütfen bir değer giriniz!");
+  } else {
+    addTodoUI(inputText);
+  }
+  e.preventDefault(); //başka sayfaya yönlendirmesin
+}
+
+//arayüze ekleme => Todo Giriniz butonuna yazılanlar altta liste olarak yazılsın
+function addTodoUI(newTodo) {
+  const li = document.createElement("li");
+  li.className = "list-group-item d-flex justify-content-between";
+  li.textContent = newTodo;
+
+  const a = document.createElement("a");
+  a.href = "#";
+  a.className = "delete-item";
+
+  const i = document.createElement("i");
+  i.className = "fa fa-remove";
+
+  a.appendChild(i);
+  li.appendChild(a);
+  todoList.appendChild(li);
+
+  //Todo Giriniz butonunda yazılan eklendikten sonra buton temizlensin
+  addInput.value = "";
+}
