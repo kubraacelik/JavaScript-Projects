87df0ff188e47c6ccdd99efc6cf8ec52ecc6a404
kubraacelik <kkbra.celik92@gmail.com>
2023-12-08 17:04:18 +0300

    updated

--- case2/app.js
+++ case2/app.js
@@ -43,13 +43,51 @@ function entryConfirmation() {
 //EMPLOYEE KISMI
 
 let tableData = [];
+let userId;
 
-if ((enteruserName = "kübra")) {
-  userId = 1;
-} else if ((enteruserName = "cihan")) {
-  userId = 2;
-} else {
-  userId = 3;
+function getUserId(enteruserName) {
+  if ((enteruserName = "kübra")) {
+    return 1;
+  } else if ((enteruserName = "cihan")) {
+    return 2;
+  } else {
+    return 3;
+  }
+}
+
+// localStorage'da mevcut veri varsa alınacak
+const storedData = localStorage.getItem("tableData");
+if (storedData) {
+  tableData = JSON.parse(storedData);
+  updateTable();
+}
+
+// Veriyi localStorage'a kaydetme fonksiyonu
+function saveDataToStorage() {
+  localStorage.setItem("tableData", JSON.stringify(tableData));
+}
+
+function updateTable() {
+  var table = document.getElementById("table").getElementsByTagName("tbody")[0];
+  // Tabloyu temizle
+  table.innerHTML = "";
+
+  // Her bir veriyi tabloya ekle
+  for (let i = 0; i < tableData.length; i++) {
+    var newRow = table.insertRow(table.rows.length);
+    var cell1 = newRow.insertCell(0);
+    var cell2 = newRow.insertCell(1);
+    var cell3 = newRow.insertCell(2);
+    var cell4 = newRow.insertCell(3);
+    var cell5 = newRow.insertCell(4);
+
+    // Hücrelere verileri ekle
+    cell1.innerHTML = tableData[i].id;
+    cell2.innerHTML = tableData[i].name;
+    cell3.innerHTML = tableData[i].surname;
+    cell4.innerHTML = tableData[i].sales;
+    cell5.innerHTML = "Onay Bekliyor";
+  }
 }
 
 // Formdaki verileri alan fonksiyon
@@ -58,6 +96,9 @@ function addContact() {
   let name = document.getElementById("nameInput").value;
   let surname = document.getElementById("surnameInput").value;
   let sales = document.getElementById("salesAmount").value;
+
+  // Giriş yapan kişiye göre userId'yi ayarla
+  userId = getUserId(enteruserName);
 
   //verileri dizi içinde tutmam lazım
   let data = {
@@ -68,6 +109,12 @@ function addContact() {
   };
 
   tableData.push(data);
+
+  // Veriyi localStorage'a kaydet
+  saveDataToStorage();
+
+  // Tabloyu güncelle
+  updateTable();
 
   // Yeni bir tablo satırı oluştur
   var table = document.getElementById("table").getElementsByTagName("tbody")[0];
@@ -87,72 +134,6 @@ function addContact() {
 
   // Formu sıfırla
   document.getElementById("form").reset();
-}
-
-//Satır güncelleme fonksiyonu
-function updateRow(button) {
-  var row = button.parentNode.parentNode;
-  var cells = row.getElementsByTagName("td");
-
-  var name = cells[0].innerHTML;
-  var surname = cells[1].innerHTML;
-  var sales = cells[2].innerHTML;
-
-  var nameInput = document.getElementById("nameInput").value;
-  var surnameInput = document.getElementById("surnameInput").value;
-  var salesAmount = document.getElementById("salesAmount").value;
-
-  document.getElementById("nameInput").value = name;
-  document.getElementById("surnameInput").value = surname;
-  document.getElementById("salesAmount").value = sales;
-  // document.getElementById("idInput").value = id;
-
-  cells[1].innerHTML = nameInput;
-  cells[2].innerHTML = surnameInput;
-  cells[3].innerHTML = salesAmount;
-}
-
-//Satır silme fonksiyonu
-function deleteRow(button) {
-  var row = button.parentNode.parentNode;
-  row.remove();
-}
-
-// Etiketlere çift tıklayınca tabloyu güncelleyen fonksiyon
-function renderTable() {
-  var table = document.getElementById("table").getElementsByTagName("tbody")[0];
-  table.innerHTML = "";
-
-  tableData.forEach(function (data) {
-    var newRow = table.insertRow(table.rows.length);
-    var cell1 = newRow.insertCell(0);
-    var cell2 = newRow.insertCell(1);
-    var cell3 = newRow.insertCell(2);
-    var cell4 = newRow.insertCell(3);
-    var cell5 = newRow.insertCell(4);
-
-    cell1.innerHTML = data.userId;
-    cell2.innerHTML = data.name;
-    cell3.innerHTML = data.surname;
-    cell4.innerHTML = data.sales;
-    cell5.innerHTML = "";
-  });
-}
-
-// Adları sıralama fonksiyonu
-function sortTable(column) {
-  tableData.sort((a, b) => {
-    if (column === "name") {
-      return a.name.localeCompare(b.name);
-    } else if (column === "surname") {
-      return a.surname.localeCompare(b.surname);
-    } else if (column === "sales") {
-      return a.sales.localeCompare(b.sales);
-    } else {
-      return a[column] - b[column];
-    }
-  });
-  renderTable();
 }
 
 //logout butonu
