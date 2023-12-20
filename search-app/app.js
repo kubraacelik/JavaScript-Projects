535213bd6e456006f385cfa3c47c33a9dbb5e304
kubraacelik <kkbra.celik92@gmail.com>
2023-12-20 22:15:55 +0300

    images from unsplash added

--- search-app/app.js
+++ search-app/app.js
@@ -11,13 +11,20 @@ runEventListeners();
 //event'lerin çalışacağı kısım
 function runEventListeners() {
   form.addEventListener("submit", search);
+  clearButton.addEventListener("click", clear);
+}
+
+function clear(e) {
+  // input'un içini temizledi
+  searchInput.value = "";
+  // Ekrandaki listenin elemanlarını siler
+  imageListWrapper.innerHTML = "";
 }
 
 function search(e) {
   const value = searchInput.value.trim();
 
   fetch(`https://api.unsplash.com/search/photos?query=${value}`, {
-    //${value} = inputa yazılan değere göre gelecek
     method: "GET",
     headers: {
       Authorization: "Client-ID EBZX5OoYULdQbGTzii_4OnkiXPO7P4m17bDpUHuneDA", //access key yazdık
@@ -37,6 +44,10 @@ function search(e) {
 
 //arayüze resim ekler, ekrana resimleri basacak
 function addImageToUI(url) {
+  //? Her resim geldiğinde bu oluşacak, altta bu kısmı oluşturduk
+  // <div className="card">
+  //   <img src="" alt="" />
+  // </div>
   const div = document.createElement("div");
   div.className = "card";
 
@@ -45,6 +56,6 @@ function addImageToUI(url) {
   img.height = "400";
   img.width = "400";
 
-  div.appendChild(img);
-  imageListWrapper.appendChild(div);
+  div.append(img); //img, div'in içine eklendi
+  imageListWrapper.append(div); //div, imageListWrapper'ın içine eklendi
 }
