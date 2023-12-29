1c4aeb627fdaa81512dae61b84743c5f07b2fe42
kubraacelik <kkbra.celik92@gmail.com>
2023-12-29 19:37:56 +0300

    updated buttons

--- shoppingCard/script.js
+++ shoppingCard/script.js
@@ -6,8 +6,12 @@ const cartTotal = document.querySelector(".total-value");
 const cartContent = document.querySelector(".cart-list");
 const productsDOM = document.querySelector("#products-dom");
 
+let cart = [];
+let buttonsDOM = [];
+
 class Products {
   async getProducts() {
+    //her şey yolunda giderse bu bloğa girecek
     try {
       //mockAPI'den adresi alıp buraya verdik.
       let result = await fetch(
@@ -16,6 +20,7 @@ class Products {
       let data = await result.json();
       let products = data;
       return products;
+      //sorun olursa bu bloğa girecek
     } catch (error) {
       console.log(error);
     }
@@ -25,6 +30,7 @@ class Products {
 class UI {
   //API'deki resimleri ekrana basıyoruz, ekrandaki kısmın adına displayProducts dedik.
   displayProducts(products) {
+    //1 resim için ayrılan alan
     let result = "";
     products.forEach((item) => {
       result += `
@@ -46,15 +52,135 @@ class UI {
     });
     productsDOM.innerHTML = result;
   }
+
+  getBagButtons() {
+    //sepete ekle butonunu seçtik
+    const buttons = [...document.querySelectorAll(".btn-add-to-card")];
+    //butonların hepsini buttonsDOM'a ekledim
+    buttonsDOM = buttons;
+    buttons.forEach((button) => {
+      let id = button.dataset.id;
+      let inCart = cart.find((item) => item.id === id);
+      //Eğer sepete ekleyeceğim ürün önceden eklenmişse tekrar eklenmesin
+      if (inCart) {
+        button.setAttribute("disabled", "disabled");
+        button.opacity = ".3";
+      }
+      //Öneceden eklenmemişse ekle
+      else {
+        button.addEventListener("click", (event) => {
+          //Butona click event'i yapıldığı için birdaha seçilemesin
+          event.target.disabled = true;
+          //Butona basıldığı için şeffaflaşsın
+          event.target.style.opacity = ".3";
+
+          //ekle butonuna basılan ürünü cartItem nesnesi içinde tutuyoruz, amount:1 1 adet eklendi demek
+          let cartItem = { ...Storage.getProduct(id), amount: 1 };
+          //cartItem'i cart dizisinde tutuyoruz
+          cart = [...cart, cartItem];
+          //cart dizisini local storage'a kaydediyoruz
+          Storage.saveCart(cart);
+          //yukarıda her bir eklenilen kartı saveCartValues'e gönder
+          this.saveCartValue(cart);
+          //eklenen her bir elemanı sepet ögesinde göster
+          this.addCartItem(cartItem);
+          //sepet butonuna tıklanınca açılsın
+          this.showCart();
+        });
+      }
+    });
+  }
+
+  saveCartValue(cart) {
+    //sepetteki toplam tutar
+    let tempTotal = 0;
+    //sepetin üstünde yazan sayı
+    let itemsTotal = 0;
+
+    cart.map((item) => {
+      tempTotal += item.price * item.amount;
+      itemsTotal += item.amount;
+    });
+
+    //Sepetteki Subtotal kısmına tempTotal sayısını ekle. Virgülden sonra 2 sayı görünsün
+    cartTotal.innerText = parseFloat(tempTotal.toFixed(2));
+    cartItems.innerText = itemsTotal;
+  }
+
+  addCartItem(item) {
+    const li = document.createElement("li");
+    li.classList.add("cart-list-item");
+    li.innerHTML = `
+        <div class="cart-left">
+        <div class="cart-left-image">
+            <img src="${item.image}" alt="product">
+        </div>
+        <div class="cart-left-info">
+            <a class="cart-left-info-title" href="#">${item.title}</a>
+            <span class="cart-left-info-price">$ ${item.price}</span>
+        </div>
+        </div>
+        <div class="cart-right">
+          <div class="cart-right-quantity">
+              <button class="quantity-minus" data-id=${item.id}>
+                  <i class="fa-solid fa-minus"></i>
+              </button>
+              <span class="quantity">${item.amount}</span>
+                  <button class="quantity-plus" data-id=${item.id}>
+                      <i class="fa-solid fa-plus"></i>
+                  </button>
+            </div>
+            <div class="cart-right-remove">
+                <button class="cart-remove-btn" data-id=${item.id}>
+                    <i class="fa-solid fa-trash"></i>
+                </button>
+            </div>
+          </div>
+    `;
+    //li'yi cartContent'e ekledik
+    cartContent.appendChild(li);
+  }
+
+  showCart() {
+    cartBtn.click();
+  }
 }
 
-class Storage {}
+//Altta new'lememek için static olarak oluşturacağız.
+//Sayfa yüklenir yüklenmez application kısmındaki veriler otomatik olarak eklensin
+class Storage {
+  static saveProducts(products) {
+    localStorage.setItem("products", JSON.stringify(products));
+  }
 
+  //id'ye göre işlem yapmak için
+  static getProduct(id) {
+    //localStorage'deki verileri getirip products içine attık
+    let products = JSON.parse(localStorage.getItem("products"));
+    //bunun içindeki her bir id eğer buton kısmındaki id'ye eşitse bunu dön
+    return products.find((product) => product.id === id);
+  }
+
+  static saveCart(cart) {
+    localStorage.setItem("cart", JSON.stringify(cart));
+  }
+}
+
+//Sayfa yüklendiğinde gerçekleşecek olaylar
 document.addEventListener("DOMContentLoaded", () => {
   const ui = new UI();
   const products = new Products();
 
-  products.getProducts().then((products) => {
-    ui.displayProducts(products);
-  });
+  //Sayfa yüklendiğinde 1-Resimler ekrana gelsin
+  //                    2-Resimler local storage'e kaydedilsin
+  //                    3-Butonlar çalışsın
+  products
+    .getProducts()
+    .then((products) => {
+      ui.displayProducts(products);
+      Storage.saveProducts(products);
+    })
+    .then(() => {
+      ui.getBagButtons();
+    });
 });
